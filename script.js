305919aeb5d104f10e4554a090284757a2608770
Murad <44784608+Muker2@users.noreply.github.com>
2024-05-05 13:00:27 +0200

    Upload Starting file

---
+++ script.js
@@ -0,0 +1,28 @@
+const filter = Array.from(document.getElementsByClassName("filter"));
+const product = Array.from(document.querySelectorAll(".grid-item"));
+
+product.forEach(prod =>
+    prod.style.display =""
+);
+
+filter.forEach(item => 
+    item.addEventListener("change", function(){
+        if(item.checked){
+            product.forEach(prod => {
+                if(item.parentElement.textContent.trim().toLowerCase() !== prod.dataset.productType.trim().toLowerCase()){
+                    prod.style.display = "none";
+                } else {
+                    prod.style.display = "";
+                }
+            });
+        }else{
+            product.forEach(prod =>
+                prod.style.display =""
+            );
+        }
+    })
+);
+
+
+
+
