a44e618478619871e14f4ea09f254aa690f2ce7a
Murad Kerimov <44784608+Muker2@users.noreply.github.com>
2024-05-09 14:39:35 +0200

    Add Eventlistener to Buttons

--- script.js
+++ script.js
@@ -1,47 +1,67 @@
-const API = "https://dummyjson.com/products?skip=0&limit=10";
 const pic = "images/Laptop.jpg";
 const desc = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed elementum nisl ac sagittis porttitor";
 const shoppingList = document.querySelector(".cart");
 const shoppingItems = [];
+const cartBtn = document.querySelectorAll(".add");
 
-fetch(API)
-  .then(response => response.json())
-  .then(data => {
-    console.log(data);
-    const products = data.products || [];
-    products.forEach(product => {
-        const template = `<div class="grid-item" data-product-type="Notebook">
+document.addEventListener('DOMContentLoaded', function() {
+    document.body.addEventListener('click', function(event) {
+        if (event.target.classList.contains('add')) {
+            alert('hello');
+        }
+    });
+});
+
+
+fetch('https://dummyjson.com/auth/refresh', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+    'Authorization': 'Bearer /* YOUR_TOKEN_HERE */', 
+  },
+  body: JSON.stringify({
+    expiresInMins: 30, // optional, defaults to 60
+  })
+})
+.then(res => res.json())
+.then(console.log);
+
+function fetchData(){
+    const API = "https://dummyjson.com/products?skip=0&limit=10";
+    return fetch(API).then(response =>{
+        if(!response.ok){
+            throw new Error ("Could not load data");
+        }
+        else {
+            return response.json()
+        }
+    }).then(data => data.products || [])
+}
+
+function displayProducts(){
+    fetchData().then(items => {
+        items.forEach(product => {
+            const template = `<div class="grid-item" data-product-type="Notebook">
         <div class="prod-img"> <img src=${pic} alt=""></div>
         <div class="grid-item-info">
             <div class="grid-item-header">
                 <h2>${product.title}</h2>
-                <div class="rating">
-                <span class="fa fa-star checked"></span>
-                <span class="fa fa-star checked"></span>
-                <span class="fa fa-star checked"></span>
-                <span class="fa fa-star"></span>
-                <span class="fa fa-star"></span>
-                </div>
+            </div>
+            <div class="grid-item-desc">
+            <p>${desc}</p>
             </div>
         </div>
         <div class="grid-price">
             <p>${product.price}€</p>
-            <button>Add to Cart</button>
+            <button class="add">Add to Cart</button>
         </div>
     </div>`;
-        document.querySelector(".grid").insertAdjacentHTML("beforeend", template);
 
+            document.querySelector(".grid").insertAdjacentHTML("beforeend", template);
+            
     });
-  })
-  .catch(error => console.error('Error:', error));
+});
+}
 
-  function addItem(item){
-        document.createElement("li");
-  }
-
-
-
-
-
-
+displayProducts();
 
